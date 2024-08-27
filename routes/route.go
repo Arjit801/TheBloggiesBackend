4a27dc5dc7b6a9870af9019bbb9c866b64d4c2b1
Arjit Gupta <84505785+Arjit801@users.noreply.github.com>
2024-08-27 19:13:40 +0530

    Add files via upload

--- routes/route.go
+++ routes/route.go
@@ -2,14 +2,13 @@ package routes
 
 import (
 	"github.com/Arjit801/TheBloggies/controller"
-	"github.com/Arjit801/TheBloggies/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
 func Setup(app *fiber.App) {
 	app.Post("/api/register", controller.Register)
 	app.Post("/api/login", controller.Login)
-	app.Use(middleware.IsAuthenticated)
+	// app.Use(middleware.IsAuthenticated)
 	app.Post("/api/post", controller.CreatPost)
 	app.Get("/api/getallpost", controller.GetAllPost)
 	app.Get("/api/getallpost/:id", controller.DetailPost)
